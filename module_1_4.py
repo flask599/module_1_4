941458f1bba4184562eb77fa21ffd2419c77e04a
flask599 <flask599@gmail.com>
2024-11-26 15:13:13 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+home_work = '12'
+hours = '1.5'
+course = 'Python'
+time = float(hours)/int (home_work)
+print ('Курс: ',course,', всего задач:',home_work,', затрачено часов:',hours, ", среднее время выполнения ",time)
